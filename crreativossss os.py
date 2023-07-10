bb45b874597fd7c8cde607f3e456b9a17f73c2e6
Alvaro Sanchez <137190598+Kasszzzzzz@users.noreply.github.com>
2023-07-10 11:15:56 -0400

    Add files via upload

---
+++ crreativossss os.py
@@ -0,0 +1,84 @@
+import os
+os.system ("cls")
+
+platinum = 0
+gold = 0
+Silver = 0
+opcEntrada = 0
+menu = 0
+Salir = 0
+compraEntrada = 0
+Ubicacion = 0
+Asistentes = 0
+rut = 0
+
+while True:
+    try:
+        opc = int(input(menu))
+        if opc == 0:
+            break
+        elif opc ==1:
+            compraEntrada()
+        elif opc ==2:
+            Ubicacion()
+        elif opc == 3:
+            Asistentes
+        elif opc ==4:
+            Salir
+
+      
+      try:
+        print("MENU DE CREATIVOS.CL , HOY MICHAEL JAM")
+    print("""
+    1|Compra de entradas
+    2|Mostar ubicaciones disponibles
+    3|Ver Listado de asistentes
+    4|Mostar ganancias totales
+    5|Salir del menu""")
+
+while True:
+    print("A escogido comprar entradas, estas son las disponibles")
+    print("""
+    1|Platinum $120.000
+    2|Gold$80.000
+    3|Silver $50.000""")
+    try:
+    
+        opcEntrada = int(input("Ingrese su opcion deseada."))
+        if opcEntrada == 1:
+            print("A comprado entrada Platinum por $120.000")
+            platinum = True
+            break
+        elif opcEntrada == 2:
+            print ("A comprado la entrada Gold por $80.000")
+            gold = False
+            break
+        elif opcEntrada == 3:
+            print ("A comprado la entrada Silver por $50.000")
+            break
+        else:
+            print("Opcion invalida.")
+    except:
+        print("Excepcion de entradas")
+
+while True:
+    print("2|Mostrar ubicaciones disponibles del escenario.")
+    try:
+        else:
+    except:
+
+    while True:
+        print("""3|Listado de asistentes.""")       
+     
+
+      
+
+
+
+
+        
+
+    
+
+
+    
